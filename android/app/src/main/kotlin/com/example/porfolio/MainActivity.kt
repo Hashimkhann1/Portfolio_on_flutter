00e3c5d1d251263183af087f359a63543db29308
Shahab Mustafa <shahabmustafa57@gmail.com>
2023-01-04 20:11:36 +0500

    Potfolio update Home Screen & About Screen

---
+++ android/app/src/main/kotlin/com/example/porfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.porfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Web/web_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class WebPage extends StatefulWidget {
+  const WebPage({Key? key}) : super(key: key);
+
+  @override
+  State<WebPage> createState() => _WebPageState();
+}
+
+class _WebPageState extends State<WebPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: ListView(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
